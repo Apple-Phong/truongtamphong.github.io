846c0cb695a8611ef7aafd443d1ce6629e8e16a8
System Administrator <tuansaker868@gmail.com>
2023-07-07 18:45:15 +0800

    -

--- Deb/HoangDinhPhong.Mix_1.0.0_iphoneos-arm/var/mobile/Library/Widgets/Homescreen/Mix/Scripts/Js/Fonts.js
+++
@@ -1,4 +0,0 @@
-let FontFamily = new FontFace("FontSelect", `url(${'/Fonts/' + config.FontSelect + '.ttf'}) format("truetype")`);
-FontFamily.load().then(function (loadedFontFamily) {
-document.fonts.add(loadedFontFamily);
-})
